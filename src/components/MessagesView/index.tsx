5aabc79e4763436873190c879a52dae620156ac3
Wender Barbosa <wenblack012@gmail.com>
2023-05-17 14:27:36 -0300

    Refactor sendPuse Effect🛠

--- src/components/MessagesView/index.tsx
+++ src/components/MessagesView/index.tsx
@@ -25,7 +25,7 @@ export function MessagesView({ children }: ViewProps) {
                 console.log(message)
 
             });
-    }, [saveData]);
+    }, []);
 
     if (isLoading) return <LoadingView />
 
